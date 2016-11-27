9200ade1b4a0894de6efc9684e4a5a91b7b79e32
volkodav1985 <volkodav_85@list.ru>
2016-11-27 18:19:42 -0500

    Week 7(1) last update groups
    Video 4

--- group.py
+++ group.py
@@ -1,45 +1,121 @@
+
+
 from model.group import Group
-import random
-import string
-import os.path
-import jsonpickle
-import getopt
-import sys
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
-except getopt.Getopterror as err:
-    getopt.usage()
-    sys.exit(2)
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
 
 
-n = 5
-f = "data/groups.json"
+    def open_groups_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php")and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
 
 
-for o, a in opts:
-    if o == "-n":
-        n = int(a)
-    elif o == "-f":
-        f = a
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s]" % id).click()
+
+
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def delete_group_by_id(self,id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
 
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(n)
+    def modify_group_by_index(self, index, new_group_date):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
+        #open modificaion
+        wd.find_element_by_name("edit").click()
+        #fill group form
+        self.fill_group_form(new_group_date)
+        #submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
 
-]
 
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
 
 
-with open(file, "w") as out:
-    jsonpickle.set_encoder_options("json",indent=2)
-    out.write(jsonpickle.encode(testdata))
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+    group_cache = None
+
+
+    def get_group_list(self):
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
---
+++ test_del_group.py
@@ -0,0 +1,15 @@
+
+
+from model.group import Group
+import random
+
+def test_delete_some_group(app,db):
+    if len (db.get_group_list()) ==0:
+        app.group.create(Group(name="test"))
+    old_groups = db.get_group_list()
+    group=random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(group)
+    assert old_groups == new_groups
