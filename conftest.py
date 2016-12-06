65e71cd217524ca79b9949078776a46854243987
volkodav1985 <volkodav_85@list.ru>
2016-12-06 08:36:46 -0500

    Week 7(2) groups and contacts

--- conftest.py
+++ conftest.py
@@ -49,9 +49,17 @@ def stop(request):
     return fixture
 
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
+
+
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action= "store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 
 
